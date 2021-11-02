d5bdb12f790196b9b3ea4d3942d3cd8e629d8533
Rick Buter <rick.buter@wearetriple.com>
2021-11-02 16:19:39 +0100

    feat: create match-summary post type and create template

--- single-pupil_of_the_week.php
+++ single-pupil_of_the_week.php
@@ -3,11 +3,6 @@
 <main class="page pvdw">
     <div class="container">
         <h1>Pupil van de week: <?php the_title(); ?></h1>
-        
-        <div class="content wysiwyg g-4 g-l-8">
-            <?php the_sub_field('10_vragen_aan'); ?>
-        </div>
-
     </div>
     <div class="container grid">
         <div class="g-l-4 g-2 padding--right">
